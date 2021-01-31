1509fd8a95b848236e0938ddcb2c448d123024ce
Weerapat Techaudomtaworn <gotcha5009@hotmail.com>
2021-01-31 22:28:00 +0200

    api POST and PUT

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,87 @@
 'use strict';
 
-module.exports = function (app) {
+const { ObjectId } = require("mongodb");
+
+module.exports = function (app, client) {
 
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+
+    .get(function (req, res) {
       let project = req.params.project;
-      
+
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    .post(async function (req, res) {
+      try {
+        if (!req.body.issue_title || !req.body.issue_text || !req.body.created_by) {
+          res.json({
+            error: "required field(s) missing"
+          });
+        } else {
+          let project = req.params.project;
+          let collection = client.db('database').collection(project);
+          let issue = {
+            assigned_to: req.body.assigned_to || "",
+            status_text: req.body.status_text || "",
+            open: true,
+            _id: new ObjectId(),
+            issue_title: req.body.issue_title,
+            issue_text: req.body.issue_text,
+            created_by: req.body.created_by,
+            created_on: new Date().toISOString(),
+            updated_on: new Date().toISOString()
+          };
+          const result = await collection.insertOne(issue);
+          console.log(result.ops[0])
+          res.send(result.ops[0]);
+        }
+      } catch (err) {
+        throw err;
+      }
+
     })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
+
+    .put(async function (req, res) {
+      try {
+        if (!req.body._id) {
+          res.json({ error: 'missing _id' });
+        } else if (!req.body.issue_title && !req.body.issue_text && !req.body.created_by && !req.body.assigned_to & !req.body.status_text) {
+          res.json({ error: 'no update field(s) sent', '_id': req.body._id });
+        } else {
+          let project = req.params.project;
+          let collection = client.db('database').collection(project);
+          let doc = await collection.findOne({ _id: ObjectId(req.body._id) });
+          console.log(doc);
+          if (doc === null) {
+            res.json({ error: 'could not update', '_id': req.body._id });
+          } else {
+            doc.issue_title = req.body.issue_title || doc.issue_title;
+            doc.issue_text = req.body.issue_text || doc.issue_text;
+            doc.created_by = req.body.created_by || doc.created_by;
+            doc.assigned_to = req.body.assigned_to || doc.assigned_to;
+            doc.status_text = req.body.status_text || doc.status_text;
+            doc.updated_on = new Date().toISOString();
+            console.log(doc);
+            collection.save(doc, (err, result) => {
+              if (err) {
+                throw err;
+              } else {
+                res.json({ result: 'successfully updated', '_id': req.body._id });
+              }
+            })
+          };
+        }
+      } catch (err) {
+        res.json({ error: 'could not update', '_id': req.body._id });
+        throw err;
+      }
+
+
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
+
     });
-    
+
 };
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai').expect;
+const cors = require('cors');
+const { MongoClient } = require("mongodb");
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 let app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 
 
@@ -33,34 +34,49 @@ app.route('/')
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
+//database client
+const client = new MongoClient(process.env.db, { useNewUrlParser: true, useUnifiedTopology: true });
+
 //For FCC testing purposes
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
+apiRoutes(app, client);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        let error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+//connect to database then start listening
+
+client.connect(err => {
+  try {
+    console.log('connected to database');
+    app.listen(process.env.PORT || 3000, function () {
+      console.log("Listening on port " + process.env.PORT);
+      if (process.env.NODE_ENV === 'test') {
+        console.log('Running Tests...');
+        setTimeout(function () {
+          try {
+            runner.run();
+          } catch (e) {
+            let error = e;
+            console.log('Tests are not valid:');
+            console.log(error);
+          }
+        }, 3500);
       }
-    }, 3500);
+    });
+
+    // perform actions on the collection object
+  } catch (err) {
+    throw err;
   }
 });
+//Start our server and tests!
+
 
 module.exports = app; //for testing
